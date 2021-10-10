e3ea1eae8c7ce1536c4a61ca05ce171450f50add
ebcms <1540837821@qq.com>
2021-10-10 10:49:56 +0800

    1

--- src/hook/app.exception/reg.php
+++ src/hook/app.exception/reg.php
@@ -1,21 +1,16 @@
 <?php
 
-/**
- * @var Throwable $th
- */
-
 use Ebcms\App;
 use Ebcms\Config;
 
-$th = $params;
-
 App::getInstance()->execute(function (
+    App $app,
     Config $config
-) use ($th) {
+) {
     if ($config->get('whoops.show@ebcms/whoops', true)) {
         $whoops = new \Whoops\Run;
         $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
         $whoops->register();
-        throw $th;
+        throw $app->getException();
     }
 });
