ccea4ef858aca953a91747cb8eff0b32e8839289
ebcms <1540837821@qq.com>
2021-10-21 11:41:32 +0800

    1

--- src/hook/app.init/reg_999.php
+++ src/hook/app.init/reg_999.php
@@ -4,13 +4,11 @@ use Ebcms\App;
 use Ebcms\Config;
 
 App::getInstance()->execute(function (
-    App $app,
     Config $config
 ) {
     if ($config->get('whoops.show@ebcms/whoops', true)) {
         $whoops = new \Whoops\Run;
         $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
         $whoops->register();
-        throw $app->getException();
     }
 });
